0502698a8dd4d701f2741b6fc90182eee45adb11
litecred <litecred@mail.com>
2016-01-18 21:48:53 +0100

    Update genesis.h

--- src/genesis.h
+++ src/genesis.h
@@ -8,6 +8,6 @@
 static const uint256 hashGenesisBlock("0x000007f685c84d9a5c16fc5c7789e0ce3a75be4d9af24e6ece75618ee0f21f1f");
 static const uint256 hashGenesisBlockTestNet("");
 static const uint256 fCheckPoint1("0x0000033a3b908f46fc2f8ecc7e01d4539b110a643dd95bf44b6a99fe85f6d27b");
-
+static const uint256 fCheckPoint2("0x000000003e47623ca6da35433236fbc64acb790846f7cbd3606c6dc799a7fc8a");
 
 #endif // GENESIS_H
