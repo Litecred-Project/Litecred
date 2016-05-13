8c2bd186d5674c4073239bb6252b000fd24cde2f
lulworm <lulworm@gmail.com>
2016-05-13 18:37:19 +0000

    Update genesis.h

--- src/genesis.h
+++ src/genesis.h
@@ -8,6 +8,11 @@
 static const uint256 hashGenesisBlock("0x000007f685c84d9a5c16fc5c7789e0ce3a75be4d9af24e6ece75618ee0f21f1f");
 static const uint256 hashGenesisBlockTestNet("");
 static const uint256 fCheckPoint1("0x0000033a3b908f46fc2f8ecc7e01d4539b110a643dd95bf44b6a99fe85f6d27b");
-static const uint256 fCheckPoint2("0x000000003e47623ca6da35433236fbc64acb790846f7cbd3606c6dc799a7fc8a");
+static const uint256 fCheckPoint2("0x000000003ebfc8ab819d36419294683b87ac54d71dd46fe3fd7f99a45534ea0f");
+static const uint256 fCheckPoint3("0x000000003e47623ca6da35433236fbc64acb790846f7cbd3606c6dc799a7fc8a");
+static const uint256 fCheckPoint4("0x000000006962febf516a29d2dfca9af5c1de1caa83bca75f4f2f96845768186e");
+static const uint256 fCheckPoint5("0x000000018b558c792acf585f12ece6e7ce0de6675c0ef3b3c4b7ca7e77d517a1");
+static const uint256 fCheckPoint6("0x0000000016d0c4828c688457f120409fdd52bc54347363a686e4bdf2a92d8e0e");
+
 
 #endif // GENESIS_H
